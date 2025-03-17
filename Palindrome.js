eea83a7f2d62f5a6448f29cb81f9a57cfca44507
ravikrishnabattala <ravikrishnbattala5@gmail.com>
2025-03-17 20:30:08 +0530

    update

---
+++ Palindrome.js
@@ -0,0 +1,13 @@
+for(let i=1;i<=9;i++)
+{
+    if(i%4==0 && i%5==0){
+
+    console.log('FizzBuzz');
+
+    }else if(i%4==0){
+        console.log('Fizz');
+    }
+    else{
+        console.log('Buzz');
+    }
+}
